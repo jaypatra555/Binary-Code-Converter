6aef57e6667ad4ee3e4f88f2d3169fff514c94fa
jaypatra555 <jaypatra555@gmail.com>
2026-06-24 16:42:34 +0530

    add gmail in comment section

---
+++ script/index.js
@@ -0,0 +1,20 @@
+
+emailjs.init("cB4TC_Z3qso8UIDHi");
+
+function sendComment() {
+  emailjs.send(
+    "service_j01zjkr",
+    "template_nfylgwh",
+    {
+      name: document.getElementById("name").value,
+      comment: document.getElementById("comment").value
+    }
+  )
+  .then(() => {
+    alert("Comment sent!");
+  })
+  .catch((err) => {
+    console.log(err);
+    alert("Error sending comment");
+  });
+}
